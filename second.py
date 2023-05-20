6742ef53919b40942833a5b435eabf089af69897
errorshahir <mahirfaysal08@outlook.com>
2023-05-20 11:50:47 +0600

    python

---
+++ second.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+name = "Python"
+_ = "Shahir"
+num1 = 3
+num2 = 5
+_num = 10
+my_list = ["Alu", "Piyaj", 2000, 1.5]
+print (a)
+print (b)
+print (name)
+print (_)
+print (_num)
+print (my_list)
---
+++ third.py
@@ -0,0 +1,4 @@
+user_name = input ("Enter your name here :")
+age = input ("Enter your input here :")
+
+print ("Hello", user_name, ("!") "Your age is :", age)
